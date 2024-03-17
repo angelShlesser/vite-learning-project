960f679173f2679bd73d17be34dd2797963691a2
Vitali <angelShlesser2010@gmail.com>
2024-03-17 20:32:27 +0100

    fix homeworks29

--- src/homeworks/Homework29/Homework29.tsx
+++ src/homeworks/Homework29/Homework29.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react"
 import Counter from "../../components/Counter/Counter"
-import { CounterProps } from "../../components/Counter/types"
+import { Homework29Wrapper } from "./styles"
 
 function Homework29() {
-  const [count, setCount] = useState(0) // Инициализация состояния счетчика
+  const [count, setCount] = useState<number>(0) // Инициализация состояния счетчика
 
   // Функции для увеличения и уменьшения счетчика
   const onMinus = () => {
@@ -14,18 +14,11 @@ function Homework29() {
     setCount(prevCount => prevCount + 1)
   }
 
-  // Пропсы для компонента Counter
-  const counterProps: CounterProps = {
-    count: count, // Текущее значение счетчика
-    onMinus: onMinus, // Функция уменьшения счетчика
-    onPlus: onPlus, // Функция увеличения счетчика
-  }
-
   return (
-    <div>
+    <Homework29Wrapper>
       {/* Рендер компонента Counter с прокинутыми пропсами */}
-      <Counter {...counterProps} />
-    </div>
+      <Counter count={count} onMinus={onMinus} onPlus={onPlus} />
+    </Homework29Wrapper>
   )
 }
 
