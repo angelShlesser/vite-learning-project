da607addd2d1c5429669a657e7bb6ef818ebcd55
Vitali <angelShlesser2010@gmail.com>
2024-03-20 21:08:15 +0100

    Добавлен  компонент Counter

---
+++ src/homeworks/Homeworks30/Homeworks30.tsx
@@ -0,0 +1,12 @@
+import Counter from "../../components/CounterTwo/Counter"
+import { Homework30Wrapper } from "./styles"
+
+function Homeworks30() {
+  return (
+    <Homework30Wrapper>
+      <Counter />
+    </Homework30Wrapper>
+  )
+}
+
+export default Homeworks30
---
+++ src/homeworks/Homeworks30/styles.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled"
+
+export const Homework30Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
